f149d470545f163b83b0af71913791ae4cd27143
Mathea <matthewherring220@Gmail.com>
2020-11-17 16:37:56 +0000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/1_9_9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_9_9.exe"
+  "1_9_9.exe.manifest"
+  "1_9_9.pdb"
+  "CMakeFiles/1_9_9.dir/main.c.obj"
+  "lib1_9_9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1_9_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
